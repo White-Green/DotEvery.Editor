a7e4716def80bd801918a21df0e074e347e1615e
White-Green <43771790+White-Green@users.noreply.github.com>
2020-08-10 20:35:38 +0900

    fix ignore setting

---
+++ dotevery-editor-simple-js/src/lib.rs
@@ -0,0 +1,63 @@
+extern crate wee_alloc;
+
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+
+use dotevery_editor_lib::components::dotevery_editor::{DotEveryEditorComponent, DotEveryEditorProperties};
+use dotevery_editor_lib::logic::dotevery_editor::DotEveryEditor;
+use dotevery_editor_lib::logic::dotevery_editor_controller::{DotEveryEditorCommand, DotEveryEditorController};
+use dotevery_editor_lib::logic::program_module::{ProgramModule, ProgramModuleChildItems, ProgramModuleOption};
+use dotevery_editor_lib::logic::program_module_list::ProgramModuleList;
+use dotevery_editor_lib::clog;
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    clog!("Hello,wasm world!");
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+
+    if let Some(entry) = document.get_element_by_id("app") {
+        let props = DotEveryEditor::new(
+            ProgramModuleList::new(
+                vec![
+                    ProgramModule::new(
+                        vec![
+                            ProgramModuleOption::StringSign("VariableDefinition".to_string()),
+                            ProgramModuleOption::ProgramModule(
+                                Some(ProgramModule::new(
+                                    vec![
+                                        ProgramModuleOption::StringInput("System.Int32".to_string()),
+                                    ],
+                                    ProgramModuleChildItems::None))),
+                        ],
+                        ProgramModuleChildItems::None),
+                    ProgramModule::new(
+                        vec![
+                            ProgramModuleOption::StringSign("VariableDefinition".to_string()),
+                            ProgramModuleOption::ProgramModule(None),
+                        ],
+                        ProgramModuleChildItems::None)
+                ]));
+        App::<DotEveryEditorComponent<Controller>>::new().mount_with_props(entry, DotEveryEditorProperties::create(props));
+    } else {
+        clog!("entry point element is not found.");
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct Controller;
+
+impl DotEveryEditorController for Controller {
+    fn create(command: Callback<DotEveryEditorCommand>) -> Self where Self: Sized {
+        unimplemented!()
+    }
+
+    fn update(&mut self, command_id: Uuid, data: DotEveryEditor) {
+        unimplemented!()
+    }
+}
